31e85f4ac32e9a02f8e565ca1ea05af885ac4b1e
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-27 12:51:56 -0500

    set up folders

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Browser from './components/routes/BrowerRoutes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Browser />
+  </React.StrictMode>,
+);
