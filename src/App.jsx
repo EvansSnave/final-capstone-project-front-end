4cad89131cac9d88534c725d19a96fbfe5e1174b
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-31 10:47:11 -0500

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import RoutesApp from "./components/routes/RoutesApp";
+
 function App() {
   return (
     <div className="App">
-      hello
+      <RoutesApp />
     </div>
   );
 }
--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,18 +1,36 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios'
 
-export const currentUser = createAsyncThunk('users/currentuser', async () => {
-  const response = axios.get('http://localhost:4000/current_user', {
-    
+export const currentUser = createAsyncThunk('users/currentUser', async (token) => {
+  const response = await axios.get('http://localhost:4000/current_user', {
+    headers: {
+      Authorization: token,
+    },
   });
+  return {
+    user: {
+      id: response.id,
+      authorized: true,
+    }
+  }
 });
+
+export const loginUser = createAsyncThunk('users/loginUser', async () => {
+  const response = await axios.post('http://localhost:4000/login');
+});
+
+const initialState = {
+  info: null,
+};
 
 const usersSlice = createSlice({
   name: 'users',
-  initialState: {},
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase()
+    builder.addCase(currentUser.fulfilled, (state, action) => {
+      state.info = action.payload
+    })
   },
 });
 
