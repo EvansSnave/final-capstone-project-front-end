d1b84f85ca28f5d1408c908965ee138f66b0c7cc
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-08 20:06:10 +0100

    Update the doctor list

---
+++ src/components/pages/DoctorsList.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import listOfDoctors from '../../redux/doctors/actions/listOfDoctors';
+
+const DoctorsList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listOfDoctors());
+  }, []);
+
+  const doctors = useSelector((state) => state.doctors.doctorsList);
+  return (
+    <ul>
+      {doctors.map((doctor) => (
+        <li>
+          <p>{doctor.id}</p>
+          <p>{doctor.cityId}</p>
+          <p>{doctor.userId}</p>
+          <p>{doctor.name}</p>
+          <p>{doctor.description}</p>
+          <p>{doctor.age}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default DoctorsList;
