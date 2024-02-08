f558218ed43c005528767110f6b769a10d96b34c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-08 20:06:34 +0100

    Update the Reservation list

---
+++ src/components/pages/MyReservations.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import listOfReservations from '../../redux/reservations/actions/indexReservations';
+
+const MyReservations = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listOfReservations(7));
+  }, [dispatch]);
+
+  const reservations = useSelector((state) => state.reservations.list);
+
+  return (
+    <ul>
+      {reservations.map((reservation) => (
+        <li>
+          <p>{reservation.id}</p>
+          <p>{reservation.user_id}</p>
+          <p>{reservation.doctor_id}</p>
+          <p>{reservation.schedule_date}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MyReservations;
